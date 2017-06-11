129d3da0216d1b9dfd5e9f25c81ec0809f132ea9
Einsten42 <milne.james@gmail.com>
2017-06-11 10:50:33 -0500

    updatedb DB URI for docker

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
   nodeServers: new Array(11),
   frontends: [],
   isyversion: '',
-  database: 'mongodb://db:27017/polyglot',
+  database: 'mongodb://mongo:27017/polyglot',
   secret: 'udi-polyglot',
   polyglotIP: require('os').networkInterfaces().eth0[0].address,
   polyglotPort: null,
