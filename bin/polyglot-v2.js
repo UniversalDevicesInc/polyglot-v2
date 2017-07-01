78073e72be009f2f8fc6206dbf33cdb9a9f9db99
Einsten42 <milne.james@gmail.com>
2017-07-01 16:31:41 -0500

    Added Documentation

--- bin/polyglot-v2.js
+++ bin/polyglot-v2.js
@@ -1,9 +1,18 @@
 #!/usr/bin/env node
 
+/**
+ * Polyglot Version 2
+ * Documentation at <todo>
+ * Written by James Milne(milne.james@gmail.com)
+ */
 'use strict'
 const os = require('os')
+/**
+* All Polyglot config is loaded via the file ~/.polyglot/.env
+* This allows for easy access to configuration for multiple co-resident nodeservers if necessary
+* All nodeservers use this same file to get their base config parameters.
+*/
 const dotenv = require('dotenv').config({path: os.homedir() + '/.polyglot/.env'})
-//dotenv.load()
 const logger = require('../lib/modules/logger')
 const config = require('../lib/config/config')
 const db = require('../lib/modules/db')
@@ -13,9 +22,10 @@ const helpers = require('../lib/modules/helpers')
 
 logger.info('Starting Polyglot version 2.0')
 
-// Clustering support
+/* Clustering support */
 const CONCURRENCY = process.env.WEB_CONCURRENCY || 1
 
+/* Initial Startup */
 function main() {
   db.startService((err) => {
     if (err === 'shutdown') { return helpers.shutdown() }
@@ -24,6 +34,7 @@ function main() {
   })
 }
 
+/* Catch SIGINT/SIGTERM and exit gracefully */
 process.once('SIGINT', function () {
   logger.info('Caught SIGINT Shutting down.')
   helpers.shutdown(() => {
