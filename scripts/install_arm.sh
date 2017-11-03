db67a2913376e49e45011b617eba31b2a8c2e4b5
Einsten42 <milne.james@gmail.com>
2017-11-03 12:10:52 -0500

    updated service for install
    Former-commit-id: 0d271b352f16d5dbfe0339ce6c6bfaae8a982e5e

--- scripts/install_arm.sh
+++ scripts/install_arm.sh
@@ -9,10 +9,18 @@ $SUDO apt-get update -qy
 $SUDO apt-get install mongodb-server git python3-pip -qy
 mkdir polyglot
 cd polyglot
-wget https://github.com/Einstein42/udi-polyglotv2/raw/master/binaries/polyglot-v2-linux-armv7.tar.gz
-tar -zxf polyglot-v2-linux-armv7.tar.gz
-dir=$(pwd)
+
+armVer=`lscpu | head -1 | awk '{print $2}'`
+if [ "$armVer" = "armv6l" ]; then
+  file_string="polyglot-v2-linux-armv6"
+elif [ "$armVer" = "armv7l" ]; then
+  file_string="polyglot-v2-linux-armv7"
+else
+  file_string="polyglot-v2-linux-x64"
+fi
+
+wget https://github.com/Einstein42/udi-polyglotv2/raw/master/binaries/$file_string.tar.gz
+tar -zxf $file_string.tar.gz
 user=$(whoami)
 wget https://github.com/Einstein42/udi-polyglotv2/raw/master/scripts/polyglot-v2.service
-sed 's/<PWD>/$dir/g' polyglot-v2.service
-$SUDO mv
+sed "s|<PWD>|$(pwd)|g;s|<USER>|$(whoami)|g;s|<FILE>|$(realpath $file_string)|g" polyglot-v2.service
